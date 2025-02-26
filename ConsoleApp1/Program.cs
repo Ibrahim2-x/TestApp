7e825565260b6e996e73ea9e133eb239496a6f64
Ibrahim <Hema@gmail.com>
2025-02-26 19:19:04 +0200

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,8 +2,21 @@
 
 namespace ConsoleApp1
 {
+
+
+
+
     internal class Program
     {
+
+        static void gg()
+        {
+            Console.WriteLine("k");
+        }
+
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("new");
